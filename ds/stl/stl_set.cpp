2d0e9e1335bf586e11c911f872b24c9cd65e3771
Aryan <aryanbisshnoi@gmail.com>
2021-07-10 14:58:15 +0530

    Add set to stl

---
+++ ds/stl/stl_set.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main()
+{
+    set<int, greater<int>> s;
+    int n,i,item;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>> item;
+        s.insert(item);
+    }
+
+    set<int> :: iterator p=s.begin();
+    while(p!=s.end())
+    {
+        cout<< *p<<" ";
+        p++;
+    }
+    cout<<endl;
+    cout<< s.size()<< endl;
+    s.erase(2);
+    cout<< s.size()<< endl;
+    s.erase(1);
+    cout<< s.size()<< endl;
+
+    return 0;
+}
